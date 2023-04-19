c10165336e07a3bbe7d8361687a8dc10d5f575fd
Vankad24 <vankad24@mail.ru>
2023-04-19 23:17:29 +0800

    Add js and html

---
+++ site/js/data_source.js
@@ -0,0 +1,24 @@
+class ShitDB{
+	constructor(function_id){
+		this.link = "https://functions.yandexcloud.net/"+function_id
+	}
+
+	request(obj, callback){
+		fetch(this.link, {
+			method: 'POST',
+			body: JSON.stringify(obj),
+		})
+			.then(response => response.json())
+			.then(json => callback(json))
+	}
+
+	get_all_ids(callback){
+		
+	}
+
+}
+
+console.log("hi")
+db = new ShitDB("d4ekq68eemul40pdvpc9")
+data = {"status": "ok", "cards": [{"count": 1, "color": 2, "shape": 3, "fill": 2}, {"count": 2, "color": 2, "shape": 3, "fill": 3}, {"count": 2, "color": 3, "shape": 2, "fill": 3}, {"count": 1, "color": 2, "shape": 2, "fill": 3}, {"count": 3, "color": 2, "shape": 2, "fill": 2}, {"count": 3, "color": 1, "shape": 3, "fill": 3}, {"count": 1, "color": 1, "shape": 2, "fill": 3}, {"count": 2, "color": 3, "shape": 2, "fill": 2}, {"count": 2, "color": 1, "shape": 1, "fill": 1}, {"count": 2, "color": 2, "shape": 1, "fill": 3}, {"count": 3, "color": 3, "shape": 1, "fill": 1}, {"count": 2, "color": 1, "shape": 1, "fill": 3}]}
+db.request(data,(json)=>console.log(json))
---
+++ site/js/main.js
@@ -0,0 +1,20 @@
+// fetch("https://functions.yandexcloud.net/d4ekq68eemul40pdvpc9", {
+//   "headers": {
+//     "accept": "*/*",
+//     "accept-language": "ru,en;q=0.9",
+//     "content-type": "text/plain;charset=UTF-8",
+//     "sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"YaBrowser\";v=\"23\"",
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": "\"Windows\"",
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "cross-site"
+//   },
+//   "referrerPolicy": "strict-origin-when-cross-origin",
+//   "body": "{\"status\":\"ok\",\"cards\":[{\"count\":1,\"color\":2,\"shape\":3,\"fill\":2},{\"count\":2,\"color\":2,\"shape\":3,\"fill\":3},{\"count\":2,\"color\":3,\"shape\":2,\"fill\":3},{\"count\":1,\"color\":2,\"shape\":2,\"fill\":3},{\"count\":3,\"color\":2,\"shape\":2,\"fill\":2},{\"count\":3,\"color\":1,\"shape\":3,\"fill\":3},{\"count\":1,\"color\":1,\"shape\":2,\"fill\":3},{\"count\":2,\"color\":3,\"shape\":2,\"fill\":2},{\"count\":2,\"color\":1,\"shape\":1,\"fill\":1},{\"count\":2,\"color\":2,\"shape\":1,\"fill\":3},{\"count\":3,\"color\":3,\"shape\":1,\"fill\":1},{\"count\":2,\"color\":1,\"shape\":1,\"fill\":3}]}",
+//   "method": "POST",
+//   "mode": "cors",
+//   "credentials": "omit"
+// }).then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
